e84b3d78eeadae7db0b1d0569402471b1f4c6a4b
bommynet <mail@bommy.net>
2017-09-06 21:24:45 +0200

    Create and use simple player animation.

--- js/game/Player.js
+++ js/game/Player.js
@@ -7,7 +7,11 @@ function Player() {
     this.radius = CFG.PLAYER.radius;
     this.anchor.setTo(0.5, 0.5);
     
+    // add(identifier, [frames], fps, loop, framesNamedByNumbers)
+    this.animations.add('down', [0,1,2,3], 15, true, true);
+    
     game.add.existing(this);
+    this.animations.play('down');
 }
 
 // inherits from Phaser.Sprite
