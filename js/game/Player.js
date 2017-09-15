84939d0c6328ae62dc92e2d1fbffd12526370ee7
bommynet <mail@bommy.net>
2017-09-15 20:56:45 +0200

    Refactored player to fit semicolon-less style.

--- js/game/Player.js
+++ js/game/Player.js
@@ -2,31 +2,31 @@
 //### Entity: Player
 //#############################################################################
 function Player() {
-    Phaser.Sprite.call(this, game, 0, 0, 'player', 0);
+    Phaser.Sprite.call(this, game, 0, 0, 'player', 0)
     
     this.radius = CFG.PLAYER.radius;
-    this.anchor.setTo(0.5, 0.5);
+    this.anchor.setTo(0.5, 0.5)
     
     // add(identifier, [frames], fps, loop, framesNamedByNumbers)
-    this.animations.add('fall', CFG.PLAYER.animations.fall, 15, true, true);
-    this.animations.add('up', CFG.PLAYER.animations.up, 15, true, true);
-    this.animations.add('prepare', CFG.PLAYER.animations.prepare, 15, false, true);
+    this.animations.add('fall', CFG.PLAYER.animations.fall, 15, true, true)
+    this.animations.add('up', CFG.PLAYER.animations.up, 15, true, true)
+    this.animations.add('prepare', CFG.PLAYER.animations.prepare, 15, false, true)
     
-    game.add.existing(this);
-    this.animations.play('prepare');
+    game.add.existing(this)
+    this.animations.play('prepare')
 }
 
 // inherits from Phaser.Sprite
-Player.prototype = Object.create(Phaser.Sprite.prototype);
-Player.prototype.constructor = Player;
+Player.prototype = Object.create(Phaser.Sprite.prototype)
+Player.prototype.constructor = Player
 
 Player.prototype.moveBy = function(pixels) {
-    this.x += pixels;
+    this.x += pixels
 }
 
 Player.prototype.resetTo = function(x, y) {
-    this.x = x;
-    this.y = y;
+    this.x = x
+    this.y = y
 }
 
 Player.prototype.update = function() {
