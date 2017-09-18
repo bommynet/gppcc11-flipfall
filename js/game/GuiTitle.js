d8da2305b2473bceebfe75695b48357d4b1d6ab0
bommynet <mail@bommy.net>
2017-09-18 21:41:33 +0200

    Refactor: GuiTitle - rename title -> sprite

--- js/game/GuiTitle.js
+++ js/game/GuiTitle.js
@@ -3,27 +3,27 @@
 function GuiTitle(posX, posY) {
     
     // add sprite to game screen
-	this.title = game.add.sprite(posX, posY, 'pinfall')
+	this.sprite = game.add.sprite(posX, posY, 'pinfall')
     
     // load animations from config
     CFG.TITLE.anim.forEach((arr) => {
-        this.title.animations.add(arr.id, arr.frames, (arr.fps ? arr.fps : 15), false)
+        this.sprite.animations.add(arr.id, arr.frames, (arr.fps ? arr.fps : 15), false)
     }, this)
 
     // start animation cycle
-    this.title.animations.play('on')
+    this.sprite.animations.play('on')
     this.nextAnim = 4
 }
 
 GuiTitle.prototype.update = function() {
-    if(this.title.animations.currentAnim.isFinished) {
+    if(this.sprite.animations.currentAnim.isFinished) {
         if(this.nextAnim < 0) {
             // get a random animation and start it
             let anim_id = Bommy.Random.randomElement(CFG.TITLE.anim.slice(2,8))
-            let ani = this.title.animations.play(anim_id.id)
+            let ani = this.sprite.animations.play(anim_id.id)
             
             // after animation finished, reset timer and view
-            ani.onComplete.addOnce(() => this.title.animations.play('on'), this)
+            ani.onComplete.addOnce(() => this.sprite.animations.play('on'), this)
             this.nextAnim = 4
         } else {
             this.nextAnim -= deltaTime
