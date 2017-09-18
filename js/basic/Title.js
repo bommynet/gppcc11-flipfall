0a07e7a2c11a9a26f327a41fad6b5bd512cf613b
bommynet <mail@bommy.net>
2017-09-18 21:43:53 +0200

    Add animation to screen transition: Title -> Menu

--- js/basic/Title.js
+++ js/basic/Title.js
@@ -22,7 +22,15 @@ Title.prototype = Object.create(Phaser.State)
         this.background = game.add.image(0, 0, "bg_title")
         this.title = new GuiTitle((CFG.WIDTH - CFG.TITLE.width) / 2, (CFG.HEIGHT - CFG.TITLE.height) / 2 - 50)
         
-        game.input.keyboard.onDownCallback = () => this.startMenu()
+        game.input.keyboard.onDownCallback = () => {
+            // lets bounce title text to new position
+            let tweenOut = game.tweens.create(this.title.sprite)
+            tweenOut.to({y: (CFG.HEIGHT - CFG.TITLE.height) / 2 - 150}, 1500, Phaser.Easing.Elastic.Out)
+
+            // after animation, switch screen
+            tweenOut.onComplete.add(() => this.game.state.start("Menu"), this)
+            tweenOut.start()
+        }
         
         console.log("created Title")
     },
@@ -32,11 +40,6 @@ Title.prototype = Object.create(Phaser.State)
         deltaTime = game.time.physicsElapsedMS * 0.001
         
         this.title.update()
-    },
-
-    // go to game menu
-    p.startMenu = function() {
-        this.game.state.start("Menu")
     }
     
     
