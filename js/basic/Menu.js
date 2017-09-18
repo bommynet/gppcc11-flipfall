8515e05cba79923f91e50c7b3f825a94c6d4b715
bommynet <mail@bommy.net>
2017-09-18 21:40:31 +0200

    Refactor: Use GuiTitle for menu screen.

--- js/basic/Menu.js
+++ js/basic/Menu.js
@@ -20,9 +20,18 @@ Menu.prototype = Object.create(Phaser.State)
     // create menu screen
     p.create = function() {
         this.background = game.add.image(0, 0, "bg_menu")
+        this.title = new GuiTitle((CFG.WIDTH - CFG.TITLE.width) / 2, (CFG.HEIGHT - CFG.TITLE.height) / 2 - 150)
+        
         game.input.keyboard.onDownCallback = () => this.game.state.start("Game")
         
         console.log("created Menu")
+    },
+        
+    p.update = function() {
+        // get elapsed time since last update
+        deltaTime = game.time.physicsElapsedMS * 0.001
+        
+        this.title.update()
     }
     
     
